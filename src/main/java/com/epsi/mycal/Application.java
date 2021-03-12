56c2875b749514dc14867715b7f4a60469c56b0d
MrMarrant <georgesandalous@gmail.com>
2021-03-12 11:58:08 +0100

    ajouter une interface console pour saisir l'opération ets les valeur.

--- src/main/java/com/epsi/mycal/Application.java
+++ src/main/java/com/epsi/mycal/Application.java
@@ -1,5 +1,58 @@
 package com.epsi.mycal;
 
+import java.util.Scanner;
+
 public class Application {
-    //System.out.println("Suis je en vie ?")
+		public static void main(String[] args) {
+				int valueA;
+				int valueB;
+				int actual;
+				Calculator calc = new Calculator();
+				Scanner myObj = new Scanner(System.in);
+				for (boolean i = true; i == true;) {
+						System.out.println("Choisisez une méthode : \n");
+						System.out.println("1. Addition");
+						System.out.println("2. Soustraction");
+						System.out.println("3. Multiplication");
+						System.out.println("4. Division");
+						int choix = myObj.nextInt();
+						switch (choix) {
+								case 1:
+										System.out.println("Choisisez une valeur pour A : \n");
+										valueA = myObj.nextInt();
+										System.out.println("Choisisez une valeur pour B : \n");
+										valueB = myObj.nextInt();
+										actual = calc.add(valueA, valueB);
+										System.out.println("Résultat de l'addition :" + actual + "\n");
+										break;
+								case 2:
+										System.out.println("Choisisez une valeur pour A : \n");
+										valueA = myObj.nextInt();
+										System.out.println("Choisisez une valeur pour B : \n");
+										valueB = myObj.nextInt();
+										actual = calc.sub(valueA, valueB);
+										System.out.println("Résultat de la soustraction :" + actual + "\n");
+										break;
+								case 3:
+										System.out.println("Choisisez une valeur pour A : \n");
+										valueA = myObj.nextInt();
+										System.out.println("Choisisez une valeur pour B : \n");
+										valueB = myObj.nextInt();
+										actual = calc.mul(valueA, valueB);
+										System.out.println("Résultat de la multiplication :" + actual + "\n");
+										break;
+								case 4:
+										System.out.println("Choisisez une valeur pour A : \n");
+										valueA = myObj.nextInt();
+										System.out.println("Choisisez une valeur pour B : \n");
+										valueB = myObj.nextInt();
+										actual = calc.div(valueA, valueB);
+										System.out.println("Résultat de la division :" + actual + "\n");
+										break;
+								default:
+										System.out.println("Veuillez indiquer une valeur valide \n");
+						}
+				}
+		}
+
 }
