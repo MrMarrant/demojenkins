0173de7eea8f42a614a10b9ce9a34a102b7a7c6b
MrMarrant <georgesandalous@gmail.com>
2021-03-12 15:14:49 +0100

    changement d'indentation

--- src/main/java/com/epsi/mycal/Calculator.java
+++ src/main/java/com/epsi/mycal/Calculator.java
@@ -2,56 +2,54 @@ package com.epsi.mycal;
 
 public class Calculator {
 
-  public int add(int valueA, int valueB){
-        return valueA + valueB ;
-    }
+	public int add(int valueA, int valueB) {
+		return valueA + valueB;
+	}
 
-    public int sub(int valueA, int valueB){
-        return valueA - valueB;
-    }
+	public int sub(int valueA, int valueB) {
+		return valueA - valueB;
+	}
 
-    public int mul(int valueA, int valueB){
-        return valueA * valueB;
-    }
+	public int mul(int valueA, int valueB) {
+		return valueA * valueB;
+	}
 
-    public int div(int valueA, int valueB){
-        return valueA / valueB;
-    }
+	public int div(int valueA, int valueB) {
+		return valueA / valueB;
+	}
 
-    /**
-     * Méthode Pair // OK
-     */
-    public boolean pair(int valueA){
-        if (valueA % 2  == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+	/**
+	 * Méthode Pair // OK
+	 */
+	public boolean pair(int valueA) {
+		if (valueA % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    /**
-     * Méthode Premier // OK
-     */
-    public boolean premier(int valueA){
-        int result;
-        boolean flag = true;
-        for(int i=2; i <= valueA/2; i++){
-            //nombre est divisible par lui-meme
-            result = valueA%i;
+	/**
+	 * Méthode Premier // OK
+	 */
+	public boolean premier(int valueA) {
+		int result;
+		boolean flag = true;
+		for (int i = 2; i <= valueA / 2; i++) {
+			//nombre est divisible par lui-meme
+			result = valueA % i;
 
-            //si le reste est 0, alors arrete la boucle. Sinon continuer la boucle
-            if(result == 0){
-                flag = false;
-                break;
-            }
-        }
+			//si le reste est 0, alors arrete la boucle. Sinon continuer la boucle
+			if (result == 0) {
+				flag = false;
+				break;
+			}
+		}
 
-        if(flag){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+		if (flag) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
